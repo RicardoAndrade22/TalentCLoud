e70b8ffe1c68d5d619f0a6a512cbe1ed08d54304
RicardoAndrade22 <ricardosud_teles@gmail.com>
2023-08-23 13:09:48 -0300

    firt refactor

--- funçoes.py
+++ funçoes.py
@@ -1,23 +1,23 @@
 
-def soma(valor1, valor2):
-    total = valor1 + valor2
-    return total
+def calculadora(valor1,valor2, operacao):
+    if(operacao ==1):
+        total = valor1 + valor2
+        return total
 
+    elif(operacao ==2):
+        total = valor1 - valor2            
+        return total
 
-def subtracao(valor1, valor2):
-    total = valor1 - valor2
-    return total
+    elif(operacao ==3):
+        total = valor1 / valor2
+        return total
 
-def divisao(valor1,valor2):
-    total = valor1 / valor2
-    return total
+    elif(operacao ==4):
+        total = valor1 * valor2
+        return total
+    else: 
+        return 0
 
-def multiplicacao(valor1,valor2):
-    total = valor1 * valor2
-    return total
+print(calculadora(7,23,2))
 
-print("soma 1 e 2=", soma(1,2))
-print("subtracao 1 e 2 = ", subtracao(1,2))
-print("divisao 1 e 2 = ", divisao(1,2))
-print("multiplicacao 1 e 2 = ", multiplicacao(1,2))
-
+    
