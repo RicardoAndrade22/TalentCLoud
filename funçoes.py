41aa14c5a17d67006aeca9844b07616e84709342
RicardoAndrade22 <ricardosud_teles@gmail.com>
2023-08-23 11:50:58 -0300

    primeiro commit

---
+++ funçoes.py
@@ -0,0 +1,23 @@
+
+def soma(valor1, valor2):
+    total = valor1 + valor2
+    return total
+
+
+def subtracao(valor1, valor2):
+    total = valor1 - valor2
+    return total
+
+def divisao(valor1,valor2):
+    total = valor1 / valor2
+    return total
+
+def multiplicacao(valor1,valor2):
+    total = valor1 * valor2
+    return total
+
+print("soma 1 e 2=", soma(1,2))
+print("subtracao 1 e 2 = ", subtracao(1,2))
+print("divisao 1 e 2 = ", divisao(1,2))
+print("multiplicacao 1 e 2 = ", multiplicacao(1,2))
+
